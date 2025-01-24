f6b4ea21fc4d680ee3146db10fb35ab72dd5920a
James Boylan <ogre@boylan.net>
2025-01-24 07:26:50 -0600

    Log Rotation Examples * Updated `README.md` to contain examples on how to setup logrotation with both Linux and Windows.

--- logger.lua
+++ logger.lua
@@ -163,12 +163,13 @@ end
 
 local function on_achievement_gained(event)
 	local event_json = {}
-	event_json["name"] = game.get_player(event.player_index)
+	event_json["name"] = event.name
+	event_json["player"] = game.get_player(event.player_index)
 	event_json["event"] = "ACHIEVEMENT_GAINED"
 	event_json["achievement_name"] = event.achievement.name
 	event_json["tick"] = event.tick
 	write_game_event_json(event_json)
-	factorio_log(event_json["event"], event_json["name"] .. "by " .. event_json["admin"])
+	factorio_log(event_json["event"], event_json["name"] .. "by " .. event_json["player"])
 end
 
 
